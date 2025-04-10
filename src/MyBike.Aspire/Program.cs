eda30e6b89357823944746124fca514daed9bf12
pmozola <mozola.pawel@gmail.com>
2025-04-10 23:36:21 +0200

    move files

--- src/MyBike.Aspire/Program.cs
+++ src/MyBike.Aspire/Program.cs
@@ -1,9 +1,14 @@
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
+var api = builder.AddProject<MyBike_API>("MyBikeAPI");
 
-//var sql = builder.AddSqlServer("MyBikeSQL");
-
-builder.AddProject<MyBike_API>("MyBikeAPI");
+var ui = builder
+    .AddNpmApp("MyBikeUI", "../MyBike.UI")
+    .WithReference(api)
+    .WaitFor(api)
+    .WithHttpEndpoint(env: "PORT", port: 4201)
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
